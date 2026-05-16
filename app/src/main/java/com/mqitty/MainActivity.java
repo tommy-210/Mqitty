c58c391761f182bd4c82ba300796bc3713fbad32
tommy-210 <poggi.tommaso02@gmail.com>
2026-05-16 21:40:20 +0200

    create color palette

--- app/src/main/java/com/mqitty/MainActivity.java
+++ app/src/main/java/com/mqitty/MainActivity.java
@@ -2,6 +2,8 @@ package com.mqitty;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,18 +13,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
 
         send_btn = findViewById(R.id.send_btn);
         receive_btn = findViewById(R.id.receive_btn);
         settings_btn = findViewById(R.id.settings_btn);
 
         send_btn.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "send", Toast.LENGTH_SHORT).show();
             System.out.println("send panel");
         });
         receive_btn.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "receive", Toast.LENGTH_SHORT).show();
             System.out.println("receive panel");
         });
         settings_btn.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "settings", Toast.LENGTH_SHORT).show();
             System.out.println("settings panel");
         });
     }
